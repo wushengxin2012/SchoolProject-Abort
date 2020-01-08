604df33262ecb815023282ab5591e5560c39d0bd
wushengxin <wushengxin90246@163.com>
2020-01-08 12:02:13 +0800

    amend the panel of map point in font side

--- src/main/resources/static/js/ajax.js
+++ src/main/resources/static/js/ajax.js
@@ -23,15 +23,58 @@ var showContent = "<p>This is the content to be show, for more infomation. this 
 
 var showWindow = function (e){
 	//<!-- 显示窗口基本信息 -->
-	var sContent = '<div style="margin:0;line-height:20px;padding:2px;">' +
-				   '<img src=' + imgPath + ' alt="" style="float:right;zoom:1;overflow:hidden;width:100px;height:100px;margin-left:3px;"/>' +
-                    showContent +
-                   '</div>';
-				   
+	// var sContent = '<div style="margin:0;line-height:20px;padding:2px;">' +
+				   // '<img src=' + imgPath + ' alt="" style="float:right;zoom:1;overflow:hidden;width:100px;height:100px;margin-left:3px;"/>' +
+                    // showContent +
+                   // '</div>';
+	var sContent = 	"<div class='panel panel-default' >" +
+						"<div class='panel-body' style='margin:0; padding:0'>" + 
+							"<img class='card-img' style='float:left; width:133px; height:133px;' " +
+								  "src=" + imgPath + "/>" +
+							"<div style='overflow:hidden'>" +
+								'<ul class="list-group" style="margin-top:20px; margin:0px; border:0px">' +
+									'<li class="list-group-item" style="padding:0; border:0px">' +
+										'<div class="input-group input-group-sm input-group-md">'+
+											'<span class="input-group-addon">传感器ID</span>'+
+											'<input type="text" class="form-control" value="21" >'+
+											'<!-- <span class="input-group-addon">cm</span> -->'+
+										'</div>'+
+									'</li>'+
+									'<li class="list-group-item" style="padding:0; border:0px">' +
+										'<div class="input-group input-group-sm input-group-md">'+
+											'<span class="input-group-addon">传感器状态</span>'+
+											'<input type="button" class="form-control btn btn-primary" value="正常" >'+
+											'<!-- <span class="input-group-addon">cm</span> -->'+
+										'</div>'+
+									'</li>'+
+									'<li class="list-group-item" style="padding:0; border:0px">' +
+										'<div class="input-group input-group-sm input-group-md">'+
+											'<span class="input-group-addon">传感器类型</span>'+
+											'<input type="text" class="form-control" value="井盖传感器" >'+
+											'<!-- <span class="input-group-addon">cm</span> -->'+
+										'</div>'+
+									'</li>'+
+									'<li class="list-group-item" style="padding:0; border:0px">' +
+										'<div class="input-group input-group-sm input-group-md">'+
+											'<span class="input-group-addon">传感器位置</span>'+
+											'<input type="text" class="form-control" value="124,256" >'+
+											'<!-- <span class="input-group-addon">cm</span> -->'+
+										'</div>'+
+									'</li>'+
+									
+								'</ul>'+
+							"</div>" +
+						"</div>" +
+						"<div class='panel-footer' style='padding:0'>" +
+							"<small class='text-muted'>Manager: Someone</small>" +
+						"</div>" +
+					"</div>";
+					
+					
 	var opts = {
 		title : title,
-		width : 290,
-		height: 105,
+		width : 300,
+		height: 180,
 		enableAutoPan : true,
 	}
 	
